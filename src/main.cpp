fca902244b6b00498a7fdd6fbeb62bdc0e95d698
yuukii <sulavlamatamang@gmail.com>
2024-06-22 10:26:47 +0545

    following along with learnopengl, making useful(?) abstractions along the way.

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,74 @@
 
 #include <internal/config/version.hpp>
 
+namespace Log {
+
+namespace Color {
+constexpr auto red   = "\033[31m";
+constexpr auto reset = "\033[39;49m";
+constexpr auto green = "\033[92m";
+}    // namespace Color
+
+void errorMessage(const std::string &message)
+{
+    std::cerr << Log::Color::red << message << Log::Color::reset;
+}
+
+void successMessage(const std::string &message)
+{
+    std::clog << Log::Color::green << message << Log::Color::reset;
+}
+
+void programLinkStatus(GLuint program, std::string_view name)
+{
+    int success;
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success) {
+        char infoLog[512];
+        glGetProgramInfoLog(program, sizeof(infoLog), nullptr, infoLog);
+        Log::errorMessage(std::format("{}::LINK FAILED:\n{}\n", name, infoLog));
+        return;
+    }
+
+    Log::successMessage(std::format("{}::LINK SUCCESS\n", name));
+}
+
+void shaderCompileStatus(GLuint shader, std::string_view name)
+{
+    int success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        char infoLog[512];
+        glGetShaderInfoLog(shader, sizeof(infoLog), nullptr, infoLog);
+        Log::errorMessage(
+            std::format("{}::COMPILATION FAILED:\n{}\n", name, infoLog));
+
+        return;
+    }
+
+    Log::successMessage(std::format("{}::COMPILATION SUCCESS\n", name));
+}
+
+}    // namespace Log
+
 int main()
 {
     constexpr auto title   = renderer::internal::project::name;
     constexpr auto version = renderer::internal::project::version;
 
-    std::clog << std::format("Welcome to {}, version {}", title, version);
+    std::clog << std::format("Welcome to {}, version {}\n", title, version);
 
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW";
         return 1;
     }
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    constexpr int width  = 300;
-    constexpr int height = width;
+    constexpr int width  = 800;
+    constexpr int height = 600;
     GLFWwindow   *window =
         glfwCreateWindow(width, height, title.data(), nullptr, nullptr);
 
@@ -39,6 +90,69 @@ int main()
         return 1;
     }
 
+    glViewport(0, 0, width, height);
+    glfwSetFramebufferSizeCallback(
+        window,
+        [](GLFWwindow *window, int width, int height) -> void {
+            glViewport(0, 0, width, height);
+        });
+
+    // -1,-1, 0
+    //  1, 1, 0
+    // 0.5,-0.5, 0
+    constexpr float triangle_vertices[] = {-1, -1, 0, 1, 1, 0, 0.5, -0.5, 0};
+
+    GLuint          VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER,
+                 sizeof(triangle_vertices),
+                 triangle_vertices,
+                 GL_STATIC_DRAW);
+
+    const char *const vertexShaderSource =
+        R"(
+           #version 330 core
+           layout(location = 0) in vec3 pos;
+
+           void main() {
+               gl_Position = vec4(pos.x,pos.y,pos.z,1.0);
+           } 
+        )";
+
+    auto vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    Log::shaderCompileStatus(vertexShader, "TRIANGLE::SHADER::VERTEX");
+
+    const char *const fragmentShaderSource =
+        R"(
+            #version 330 core
+            out vec4 FragColor;
+
+            void main() {
+                FragColor = vec4(1.0,1.0,0.5,1.0);
+            }
+        )";
+    auto fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    Log::shaderCompileStatus(fragmentShader, "TRIANGLE::SHADER::FRAGMENT");
+
+    auto shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    Log::programLinkStatus(shaderProgram, "shaderProgram");
+    glUseProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
     while (!glfwWindowShouldClose(window)) {
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             glfwSetWindowShouldClose(window, GLFW_TRUE);
@@ -49,5 +163,7 @@ int main()
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
+
+    glfwTerminate();
     return 0;
 }
